6ddc157ac2658770cce71e2a4cd0fda01118cbb3
Victor Adewoye <victoradewoye2013@gmail.com>
2019-05-09 12:53:50 -0400

    Make FetchController class singleton class

--- app/src/main/java/com/victoradewoye/downloadbooster/FetchController.java
+++ app/src/main/java/com/victoradewoye/downloadbooster/FetchController.java
@@ -1,8 +1,19 @@
 package com.victoradewoye.downloadbooster;
 
 public class FetchController {
+    
+    private static FetchController instance = null;
+
 
     public FetchController() {
 
+    }
+
+    protected static FetchController shared() {
+        if (instance == null) {
+            instance = new FetchController();
+        }
+
+        return instance;
     }
 }
