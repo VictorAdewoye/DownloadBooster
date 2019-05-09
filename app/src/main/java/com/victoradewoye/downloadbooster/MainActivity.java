1b7104126e884703ef4632167327f14a2bcfac60
Victor Adewoye <victoradewoye2013@gmail.com>
2019-05-09 12:47:03 -0400

    Add MAinScreen Refactor object Views attributes for mainscreen

---
+++ app/src/main/java/com/victoradewoye/downloadbooster/MainActivity.java
@@ -0,0 +1,14 @@
+package com.victoradewoye.downloadbooster;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+}
