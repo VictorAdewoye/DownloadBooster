a5e0f33a72ca7c1fea4b9f62f892c9e249cf109d
Victor Adewoye <victoradewoye2013@gmail.com>
2019-05-09 13:01:11 -0400

    Add method clickDownload to capture click action on the download button Add code to setup the fetchController class

--- app/src/main/java/com/victoradewoye/downloadbooster/MainActivity.java
+++ app/src/main/java/com/victoradewoye/downloadbooster/MainActivity.java
@@ -2,6 +2,8 @@ package com.victoradewoye.downloadbooster;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,6 +11,21 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        FetchController.shared().setUpFetchController(this);
     }
 
+    public void clickDownlaod(View view) {
+        FetchController.shared().fetchFile(new IFetchFileDownload() {
+            @Override
+            public void onComplete(Object fileObject) {
+                Toast.makeText(MainActivity.this, " Download Successful,", Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void errorOccured(String error) {
+                Toast.makeText(MainActivity.this, " Download Failed,", Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
 }
