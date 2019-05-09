419ac6db7f5a8a1109799b3f1a4074977596dc8e
Victor Adewoye <victoradewoye2013@gmail.com>
2019-05-09 12:50:06 -0400

    Create Connection Class

---
+++ app/src/main/java/com/victoradewoye/downloadbooster/Connection.java
@@ -0,0 +1,14 @@
+package com.victoradewoye.downloadbooster;
+
+import android.util.Log;
+
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+public class Connection {
+
+    public Connection() {
+        // Empty Constructor
+    }
+}
