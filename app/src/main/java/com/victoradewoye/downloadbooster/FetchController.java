5bc53e42582dfdad42b7b88739ac07e4c21e032c
Victor Adewoye <victoradewoye2013@gmail.com>
2019-05-18 18:54:54 -0400

    Add parallelFetchFile method Reorganize codes

--- app/src/main/java/com/victoradewoye/downloadbooster/FetchController.java
+++ app/src/main/java/com/victoradewoye/downloadbooster/FetchController.java
@@ -10,15 +10,20 @@ import java.io.File;
 import java.io.FileOutputStream;
 import java.io.InputStream;
 import java.io.OutputStream;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
+import java.util.concurrent.TimeUnit;
 
 
 public class FetchController {
-    private Connection connection;
-
-    private Context context;
-
     private static FetchController instance = null;
-
+    private Connection connection;
+    private Context context;
     private File fileStorageDirectory;
 
     public FetchController() {
@@ -45,14 +50,15 @@ public class FetchController {
 
     private void checkDirectory(File file) {
         if (!file.exists()) {
-            if (!file.mkdirs()); {
+            if (!file.mkdirs()) ;
+            {
                 //TODO: Handle Error
             }
         }
 
     }
 
-    public void fetchFile (final IFetchFileDownload callBack) {
+    public void fetchFile(final IFetchFileDownload callBack) {
         AsyncTask.execute(new Runnable() {
             @Override
             public void run() {
@@ -82,7 +88,7 @@ public class FetchController {
                             public void getFileResult(InputStream inputStream) {
 
                                 try {
-                                    OutputStream outputStream = outputFile.length() == 0 ? new FileOutputStream(outputFile) :  new FileOutputStream(outputFile, true);
+                                    OutputStream outputStream = outputFile.length() == 0 ? new FileOutputStream(outputFile) : new FileOutputStream(outputFile, true);
 
                                     byte[] buffer = new byte[16 * 1500];
 
@@ -103,11 +109,11 @@ public class FetchController {
                                     }
                                     outputStream.close();
 
-                                    if(outputFile.length() >= totalDownloadSize ) {
+                                    if (outputFile.length() >= totalDownloadSize) {
                                         callBack.onComplete(outputFile);
                                     }
 
-                                } catch(Exception exception) {
+                                } catch (Exception exception) {
                                     callBack.errorOccured(exception.getMessage());
                                 }
                             }
@@ -129,4 +135,118 @@ public class FetchController {
             }
         });
     }
+
+    public void parallelFetchFile(final IFetchFileDownload callBack) {
+
+        int MYTHREADS = 4;
+
+        final ExecutorService executor = Executors.newFixedThreadPool(MYTHREADS);
+
+        final List<Future<InputStream>> futures = new ArrayList<>();
+
+        AsyncTask.execute(new Runnable() {
+            @Override
+            public void run() {
+
+                final long totalDownloadSize = 4194308;
+
+                long endChunkSize = 1048577; // this the equivalent of 1MB in bytes.
+
+                long startChunkSize = 0;
+
+                String path = fileStorageDirectory.getPath();
+
+                final File outputFile = new File(path + File.separator + "384Test.jar");
+
+                for (long i = startChunkSize; i < totalDownloadSize; i += 1048577) {
+
+                    Future<InputStream> future = executor.submit(parallelRunnableObject(i, endChunkSize));
+
+                    futures.add(future);
+
+                    endChunkSize += 1048577;
+
+                }
+
+                for (Future<InputStream> f : futures) {
+                    final long outputFileSize = outputFile.length();
+                    try {
+                        OutputStream outputStream = outputFile.length() == 0 ? new FileOutputStream(outputFile) : new FileOutputStream(outputFile, true);
+
+                        byte[] buffer = new byte[16 * 1500];
+
+                        int bytesRead = 0;
+
+                        long total = outputFileSize;
+
+                        InputStream inputStream = f.get();
+
+                        if (inputStream != null) {
+
+                            while ((bytesRead = inputStream.read(buffer, 0, buffer.length)) >= 0) {
+                                outputStream.write(buffer, 0, bytesRead);
+
+                                total += bytesRead;
+
+                                Intent intent = new Intent("update");
+
+                                intent.putExtra("update_Value", total);
+
+                                LocalBroadcastManager.getInstance(context).sendBroadcast(intent);
+                            }
+
+                        }
+                        outputStream.close();
+
+                        if (outputFile.length() >= totalDownloadSize) {
+                            callBack.onComplete(outputFile);
+                        }
+
+                    } catch (InterruptedException | ExecutionException ex) {
+                        callBack.errorOccured(ex.getMessage());
+                    } catch (Exception exception) {
+                        callBack.errorOccured(exception.getMessage());
+                    }
+
+                }
+
+                try {
+                    executor.awaitTermination(Long.MAX_VALUE, TimeUnit.MILLISECONDS);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+
+                executor.shutdown();
+            }
+        });
+    }
+
+
+    private Callable<InputStream> parallelRunnableObject(final long startChunkSize, final long endChunkSize) {
+
+        Callable<InputStream> chunkRequest = new Callable<InputStream>() {
+            @Override
+            public InputStream call() throws Exception {
+                final InputStream[] chunkInputStream = {null};
+
+                connection.downloadFileByRange(new IConnectionFileDownload() {
+                    @Override
+                    public void getFileResult(InputStream inputStream) {
+
+                        chunkInputStream[0] = inputStream;
+                    }
+
+                    @Override
+                    public void errorOccured(String error) {
+                        Log.i("Report", "errorOccured: ");
+                    }
+                }, startChunkSize, endChunkSize);
+
+                return chunkInputStream[0];
+            }
+        };
+
+        return chunkRequest;
+    }
+
 }
